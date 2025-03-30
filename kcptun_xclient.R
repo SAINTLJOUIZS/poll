77c6ab3961f6a37e345842305c9f97dd85a32eb5
SAINTLJOUIZS <SAINTLJOUIZS@users.noreply.github.com>
2025-03-30 16:21:58 +0000

    Remove the #ifdef LOCORE. No assembly file includes this file since the

--- kcptun_xclient.R
+++ kcptun_xclient.R
@@ -1,15 +1,1 @@
 # Auto-generated file for poll
-
-# Update append: 1763924027
-
-# Update append: 1763924027
-
-# Update append: 1763924027
-
-# Update append: 1763924028
-
-# Update append: 1763924028
-
-# Update append: 1763924029
-
-# Update append: 1763924029
